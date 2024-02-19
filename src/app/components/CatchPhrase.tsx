70f24cea219ff32054dd5693544e3f7d1a33956a
Juan Sued <juansued19@gmail.com>
2024-02-18 23:04:31 -0300

    feature/adicionado carrossel de services

--- src/app/components/CatchPhrase.tsx
+++ src/app/components/CatchPhrase.tsx
@@ -1,7 +1,7 @@
 export default function CatchPhrase() {
   return (
     <>
-      <section className="titleIntroduction w-full flex items-center justify-center h-40">
+      <section className="titleIntroduction w-full flex items-center justify-center   pt-6">
         <h1 className=" max-w-[600px]  bg-gradient-to-b from-black dark:from-white to-rgba-white-70 drop-shadow bg-clip-text text-transparent text-3xl md:text-7xl w-full text-center  animate__animated animate__fadeInUp duration-1000 delay-1000">
           Um horizonte de <strong>POSSIBILIDADES</strong>
         </h1>
---
+++ src/app/components/Services.tsx
@@ -0,0 +1,80 @@
+import { Button } from '@/components/ui/button'
+import { Card } from '@/components/ui/card'
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel'
+import { IServiceItem, servicesList } from '@/data/services'
+
+export default function Services() {
+  return (
+    <>
+      <section className="differences  w-full flex flex-col items-center  ">
+        <div className="w-full flex justify-center md:px-24">
+          <h1 className="text-5xl font-bold drop-shadow text-start ">
+            Expertise para oferecer as melhores soluções para os seus desafios
+            de negócio
+          </h1>
+        </div>
+
+        <Carousel
+          opts={{
+            align: 'start',
+          }}
+          className=" w-[99.40vw]  "
+        >
+          <div className=" flex w-full justify-end gap-2 px-8 ">
+            <CarouselPrevious />
+            <CarouselNext />
+          </div>
+
+          <CarouselContent className=" py-12 pl-2 md:pl-20 ">
+            {servicesList.map((service, index) => (
+              <CarouselItem
+                key={index}
+                className=" basis-80 sm:basis-1/2 md:basis-1/3 lg:basis-1/4 min-h-[400px] "
+              >
+                <div className="mx-5 h-full">
+                  <CardService
+                    key={index}
+                    title={service.title}
+                    icon={service.icon}
+                    description={service.description}
+                    href={service.href}
+                  />
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </Carousel>
+      </section>
+    </>
+  )
+}
+
+function CardService({ icon, title, description, href }: IServiceItem) {
+  const link =
+    href ||
+    `https://wa.me/5521984980723?text=Ol%C3%A1%2C+gostaria+de+saber+mais+sobre+${title}`
+
+  return (
+    <Card className="hover:-translate-y-11  p-6 w-full h-full   hover:shadow-2xl   transition-all duration-500">
+      <figure className="flex flex-col h-full justify-between gap-2 ">
+        <div className="p-6 text-[#ADD8E6]  dark:text-blueTheme-500  bg-blueTheme-500 dark:bg-[#ADD8E6] w-fit rounded-full h-20">
+          {icon}
+        </div>
+
+        <figcaption className="grid gap-2 ">
+          <h3 className="text-lg font-semibold">{title}</h3>
+          <p className="text-muted-foreground">{description}</p>
+        </figcaption>
+        <a href={link}>
+          <Button variant="sucess">Saiba mais</Button>
+        </a>
+      </figure>
+    </Card>
+  )
+}
