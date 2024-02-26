e156e9bd41c119721a638356af50a78520127083
Juan Sued <juansued19@gmail.com>
2024-02-26 17:45:07 -0300

    implementadas melhorias e orbitals

--- src/data/clients.ts
+++ src/data/clients.ts
@@ -5,40 +5,23 @@ export interface IClient {
 }
 export const clientsList: IClient[] = [
   {
-    title: 'B3',
-    description:
-      '"Impulsionou negociações online, proporcionando uma experiência envolvente e atraente para investidores!"',
-    href: 'https://www.b3.com.br/pt_br/',
-  },
-  {
-    title: 'ALLOS',
+    title: 'Sassi Imóveis',
     description:
       '"Desenvolveu um site excepcional que destaca e fortalece a singularidade de nossa marca."',
-    href: 'https://allos.co/',
+    href: 'https://www.sassiimoveis.com.br/',
   },
   {
-    title: 'Petrobras',
+    title: 'dentalRio',
     description:
       '"Automatizou tarefas, proporcionando eficiência e soluções notáveis."',
-    href: 'https://petrobras.com.br/',
+    href: 'https://www.dentalrio.com.br/',
   },
+
   {
-    title: 'BMG',
-    description:
-      '"Transformou dados em ação estratégica, fornecendo insights avançados."',
-    href: 'https://www.bancobmg.com.br/',
-  },
-  {
-    title: 'Zap Recall',
-    description:
-      '"Criou Landing Page envolvente, conquistando público com maestria!"',
-    href: 'https://zap-recall-juansued.vercel.app/',
-  },
-  {
-    title: 'Natura',
+    title: 'SIMPLO',
     description:
       '"Revitalizou presença digital, promovendo essência e sustentabilidade."',
-    href: 'https://www.natura.com.br/',
+    href: 'https://blog.simplusbr.com/curso-de-mecanica-automotiva/',
   },
   {
     title: 'Empório Maria',
@@ -46,22 +29,23 @@ export const clientsList: IClient[] = [
       '"Construiu site único, destacando produtos artesanais. Parceria que impulsionou negócio local!"',
     href: 'https://emporiodamaria.com/',
   },
+
   {
-    title: 'Tech Soluções',
-    description:
-      '"Desenvolveu sistemas eficientes, otimizando processos internos. Parceria que impulsionou produtividade!"',
-    href: 'https://techsolucoes.com/',
-  },
-  {
-    title: 'Pizzaria Bairro',
+    title: 'Pizza & Cia',
     description:
       '"Transformou presença online, oferecendo experiência única para amantes de pizza. Parceria que valoriza sabor local!"',
-    href: 'https://pizzariadobairro.com/',
+    href: 'https://pizzaecia.net/pizzas/',
   },
   {
-    title: 'Pet Shop Amigo',
+    title: 'Meyer PetShop',
     description:
-      '"Modernizou presença digital, proporcionando praticidade e carinho para pets. Parceria que fortalece laços!"',
-    href: 'https://petshopamigofiel.com/',
+      '"Modernizou nossa presença digital, proporcionando praticidade e carinho para pets. Parceria que fortalece laços!"',
+    href: 'https://meyerpetshop.com.br/',
+  },
+  {
+    title: 'EMPÓRIO ARGIANO',
+    description:
+      '"Desenvolveu sistemas eficientes, otimizando processos internos. Parceria que impulsionou produtividade!"',
+    href: 'https://www.emporioargiano.com.br/',
   },
 ]
